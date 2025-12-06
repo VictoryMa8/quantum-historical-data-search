54e4efa46039982afea01a162a01827c8992d8c7
Victory Ma <victoryma23@gmail.com>
2025-12-05 21:27:53 -0600

    Changed var name

--- main.py
+++ main.py
@@ -5,8 +5,10 @@ parquet_file_path = "data.parquet"
 csv_file_path = "data.csv"
 
 # Read the Parquet file into a pandas dataframe
-dataframe = pd.read_parquet("hf://datasets/burgerbee/history_wiki/data/train-00000-of-00001.parquet")
+
+# A dataset on wikipedia entries regarding historical terms or concepts
+wiki_df = pd.read_parquet("hf://datasets/burgerbee/history_wiki/data/train-00000-of-00001.parquet")
 
 # Write the dataframe into a CSV file
 # Setting index to False to prevent writing the dataframe index as a column in the CSV
-dataframe.to_csv(csv_file_path, index=False)
+wiki_df.to_csv(csv_file_path, index=False)
